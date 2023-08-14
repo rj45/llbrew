bb0db3c9e5e22d5b23d61fd9ac70afe4f408f995
rj45 <ryan@rj45.ca>
2023-08-14 13:08:49 -0700

    Remove accidentally left in debug statements

--- xform/legalization/clobbercopies.go
+++ xform/legalization/clobbercopies.go
@@ -1,8 +1,6 @@
 package legalization
 
 import (
-	"fmt"
-
 	"github.com/rj45/llbrew/ir"
 	"github.com/rj45/llbrew/ir/op"
 	"github.com/rj45/llbrew/xform"
@@ -28,11 +26,9 @@ func addClobberCopies(it ir.Iter) {
 	cand := def.Instr()
 	if cand.Op != nil && cand.Op.IsCopy() && cand.NumDefs() == 1 && cand.Def(0).NumUses() == 1 && cand.Block() == instr.Block() {
 		// already added the copy
-		fmt.Println("copy already added for: ", instr.LongString(), "copy:", cand.LongString())
 		return
 	}
 
 	cp := it.Insert(op.Copy, instr.Arg(0).Type, instr.Arg(0))
-	fmt.Println("inserting copy for: ", instr.LongString(), "copy:", cp.LongString())
 	instr.ReplaceArg(0, cp.Def(0))
 }
